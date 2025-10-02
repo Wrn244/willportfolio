6c3ec2fb856e9c4efb32895fdf8e9c8270aa0f18
Will Nelson <will@moonlightnstuff.com>
2025-10-02 16:51:02 -0400

    Updates navbar for project deployment
    Updates the navbar component to correctly handle relative paths when the site is deployed as a GitHub Pages project.
    
    Dynamically determines the base path based on the hostname and URL structure, ensuring links work correctly in both local development and deployed environments.

--- components/navbar-component.js
+++ components/navbar-component.js
@@ -1,26 +1,24 @@
 class MyNavbar extends HTMLElement {
   connectedCallback() {
+
+    const repoName = 'willportfolio'; // change if repo name changes
+    const pathSegments = window.location.pathname.split('/').filter(Boolean);
+    const isProjectSite = window.location.hostname.endsWith('github.io') || pathSegments.includes(repoName);
+    const base = isProjectSite ? `/${repoName}/` : '/';
+
+    const href = (p) => (base + p).replace(/\/{2,}/g, '/'); // avoids double-slashes    
+
     this.innerHTML = `
     <section class="nav">
         <div id="nav-container">
             <header class="container">
                 <ul class="nav nav-pills">
-                    <li class="nav-item">
-                        <a href="/" class="nav-link">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../about/index.html" class="nav-link">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../projects" class="nav-link">Projects</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../resume" class="nav-link" style="color:none">Resume</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="../contact" class="nav-link">Contact</a>
-                    </li>
-                </ul>
+                    <li class="nav-item"><a href="${href('index.html')}" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="${href('about/index.html')}" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="${href('projects/index.html')}" class="nav-link">Projects</a></li>
+                    <li class="nav-item"><a href="${href('resume/index.html')}" class="nav-link">Resume</a></li>
+                    <li class="nav-item"><a href="${href('contact/index.html')}" class="nav-link">Contact</a></li>
+            </ul>
             </header>
         </div>
     </section>
